3a0da27126e18ac4053145e00b7b5d3387c55747
Nicolás Cuitiño <nicolasignacio931@gmail.com>
2024-10-07 15:23:33 -0300

    Administrar proveedores validado, para RUT, número de cuenta, línea de crédito, número de teléfono y correo electrónico. Además, se incluyo un nuevo js para las nuevas validaciones.

--- modelos/proveedores.modelo.php
+++ modelos/proveedores.modelo.php
@@ -2,144 +2,147 @@
 
 require_once "conexion.php";
 
-class ModeloProveedores{
+class ModeloProveedores
+{
+    /*=============================================
+    CREAR PROVEEDOR
+    =============================================*/
 
-	/*=============================================
-	CREAR PROVEEDOR
-	=============================================*/
+    static public function mdlIngresarProveedor($tabla, $datos)
+    {
 
-	static public function mdlIngresarProveedor($tabla, $datos){
+        $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(razon_social, rut, comuna, nro_cuenta, banco, telefono, email, actividad, ejecutivo, rubros, pais, region, direccion, id_plazo) VALUES (:razon_social, :rut, :comuna, :nro_cuenta, :banco, :telefono, :email, :actividad, :ejecutivo, :rubros, :pais, :region, :direccion, :id_plazo)");
 
-		$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(razon_social, rut, comuna, nro_cuenta, banco, telefono, email, actividad, ejecutivo, rubros, pais, region, direccion, id_plazo) VALUES (:razon_social, :rut, :comuna, :nro_cuenta, :banco, :telefono, :email, :actividad, :ejecutivo, :rubros, :pais, :region, :direccion, :id_plazo)");
-
-		$stmt->bindParam(":razon_social", $datos["razon_social"], PDO::PARAM_STR);
+        $stmt->bindParam(":razon_social", $datos["razon_social"], PDO::PARAM_STR);
         $stmt->bindParam(":rut", $datos["rut"], PDO::PARAM_STR);
-		$stmt->bindParam(":comuna", $datos["comuna"], PDO::PARAM_STR);
-		$stmt->bindParam(":region", $datos["region"], PDO::PARAM_STR);
-		$stmt->bindParam(":pais", $datos["pais"], PDO::PARAM_STR);
+        $stmt->bindParam(":comuna", $datos["comuna"], PDO::PARAM_STR);
+        $stmt->bindParam(":region", $datos["region"], PDO::PARAM_STR);
+        $stmt->bindParam(":pais", $datos["pais"], PDO::PARAM_STR);
         $stmt->bindParam(":nro_cuenta", $datos["nro_cuenta"], PDO::PARAM_STR);
         $stmt->bindParam(":banco", $datos["banco"], PDO::PARAM_STR);
         $stmt->bindParam(":telefono", $datos["telefono"], PDO::PARAM_STR);
-		$stmt->bindParam(":email", $datos["email"], PDO::PARAM_STR);
-		$stmt->bindParam(":actividad", $datos["actividad"], PDO::PARAM_STR);
-		$stmt->bindParam(":ejecutivo", $datos["ejecutivo"], PDO::PARAM_STR);
-		$stmt->bindParam(":rubros", $datos["rubros"], PDO::PARAM_STR);
-		$stmt->bindParam(":direccion", $datos["direccion"], PDO::PARAM_STR);
-		$stmt->bindParam(":id_plazo", $datos["id_plazo"], PDO::PARAM_STR);				
+        $stmt->bindParam(":email", $datos["email"], PDO::PARAM_STR);
+        $stmt->bindParam(":actividad", $datos["actividad"], PDO::PARAM_STR);
+        $stmt->bindParam(":ejecutivo", $datos["ejecutivo"], PDO::PARAM_STR);
+        $stmt->bindParam(":rubros", $datos["rubros"], PDO::PARAM_STR);
+        $stmt->bindParam(":direccion", $datos["direccion"], PDO::PARAM_STR);
+        $stmt->bindParam(":id_plazo", $datos["id_plazo"], PDO::PARAM_STR);
 
-		if($stmt->execute()){
+        if ($stmt->execute()) {
 
-			return "ok";
+            return "ok";
 
-		}else{
-			return "error";
-		
-		}
+        } else {
+            return "error";
 
-		$stmt->close();
-		$stmt = null;
+        }
 
-	}
+        $stmt->close();
+        $stmt = null;
 
-	/*=============================================
-	MOSTRAR PROVEEDORES
-	=============================================*/
+    }
 
-	static public function mdlMostrarProveedores($tabla, $item, $valor){
+    /*=============================================
+    MOSTRAR PROVEEDORES
+    =============================================*/
 
-		if($item != null){
+    static public function mdlMostrarProveedores($tabla, $item, $valor)
+    {
 
-			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
+        if ($item != null) {
 
-			$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
+            $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
 
-			$stmt -> execute();
+            $stmt->bindParam(":" . $item, $valor, PDO::PARAM_STR);
 
-			return $stmt -> fetch();
+            $stmt->execute();
 
-		}else{
+            return $stmt->fetch();
 
-			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
+        } else {
 
-			$stmt -> execute();
+            $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
 
-			return $stmt -> fetchAll();
+            $stmt->execute();
 
-		}
+            return $stmt->fetchAll();
 
-		$stmt -> close();
+        }
 
-		$stmt = null;
+        $stmt->close();
 
-	}
+        $stmt = null;
 
-	/*=============================================
-	EDITAR PROVEEDOR
-	=============================================*/
+    }
 
-	static public function mdlEditarProveedor($tabla, $datos){
+    /*=============================================
+    EDITAR PROVEEDOR
+    =============================================*/
 
-		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET razon_social = :razon_social, rut = :rut, comuna = :comuna, pais = :pais, region = :region, rubros = :rubros, id_plazo = :id_plazo, direccion = :direccion, nro_cuenta = :nro_cuenta, banco = :banco, telefono = :telefono,  email = :email, actividad = :actividad, ejecutivo = :ejecutivo WHERE id = :id");
+    static public function mdlEditarProveedor($tabla, $datos)
+    {
 
-		$stmt->bindParam(":id", $datos["id"], PDO::PARAM_INT);
-		$stmt->bindParam(":razon_social", $datos["razon_social"], PDO::PARAM_STR);
+        $stmt = Conexion::conectar()->prepare("UPDATE $tabla SET razon_social = :razon_social, rut = :rut, comuna = :comuna, pais = :pais, region = :region, rubros = :rubros, id_plazo = :id_plazo, direccion = :direccion, nro_cuenta = :nro_cuenta, banco = :banco, telefono = :telefono,  email = :email, actividad = :actividad, ejecutivo = :ejecutivo WHERE id = :id");
+
+        $stmt->bindParam(":id", $datos["id"], PDO::PARAM_INT);
+        $stmt->bindParam(":razon_social", $datos["razon_social"], PDO::PARAM_STR);
         $stmt->bindParam(":rut", $datos["rut"], PDO::PARAM_STR);
-		$stmt->bindParam(":comuna", $datos["comuna"], PDO::PARAM_STR);
-		$stmt->bindParam(":region", $datos["region"], PDO::PARAM_STR);
-		$stmt->bindParam(":pais", $datos["pais"], PDO::PARAM_STR);
+        $stmt->bindParam(":comuna", $datos["comuna"], PDO::PARAM_STR);
+        $stmt->bindParam(":region", $datos["region"], PDO::PARAM_STR);
+        $stmt->bindParam(":pais", $datos["pais"], PDO::PARAM_STR);
         $stmt->bindParam(":nro_cuenta", $datos["nro_cuenta"], PDO::PARAM_STR);
         $stmt->bindParam(":banco", $datos["banco"], PDO::PARAM_STR);
         $stmt->bindParam(":telefono", $datos["telefono"], PDO::PARAM_STR);
-		$stmt->bindParam(":email", $datos["email"], PDO::PARAM_STR);
-		$stmt->bindParam(":actividad", $datos["actividad"], PDO::PARAM_STR);
-		$stmt->bindParam(":ejecutivo", $datos["ejecutivo"], PDO::PARAM_STR);
-		$stmt->bindParam(":rubros", $datos["rubros"], PDO::PARAM_STR);
-		$stmt->bindParam(":direccion", $datos["direccion"], PDO::PARAM_STR);
-		$stmt->bindParam(":id_plazo", $datos["id_plazo"], PDO::PARAM_STR);				
+        $stmt->bindParam(":email", $datos["email"], PDO::PARAM_STR);
+        $stmt->bindParam(":actividad", $datos["actividad"], PDO::PARAM_STR);
+        $stmt->bindParam(":ejecutivo", $datos["ejecutivo"], PDO::PARAM_STR);
+        $stmt->bindParam(":rubros", $datos["rubros"], PDO::PARAM_STR);
+        $stmt->bindParam(":direccion", $datos["direccion"], PDO::PARAM_STR);
+        $stmt->bindParam(":id_plazo", $datos["id_plazo"], PDO::PARAM_STR);
 
-			
 
-		if($stmt->execute()){
+        if ($stmt->execute()) {
 
-			return "ok";
+            return "ok";
 
-		}else{
+        } else {
 
-			return "error";
-		
-		}
+            return "error";
 
-		$stmt -> close();
+        }
 
-		$stmt = null;
+        $stmt->close();
 
-	}
+        $stmt = null;
 
-	/*=============================================
-	BORRAR PROVEEDOR
-	=============================================*/
+    }
 
-	static public function mdlBorrarProveedor($tabla, $datos){
+    /*=============================================
+    BORRAR PROVEEDOR
+    =============================================*/
 
-		$stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id = :id");
+    static public function mdlBorrarProveedor($tabla, $datos)
+    {
 
-		$stmt -> bindParam(":id", $datos, PDO::PARAM_INT);
+        $stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id = :id");
 
-		if($stmt -> execute()){
+        $stmt->bindParam(":id", $datos, PDO::PARAM_INT);
 
-			return "ok";
-		
-		}else{
+        if ($stmt->execute()) {
 
-			return "error";	
+            return "ok";
 
-		}
+        } else {
 
-		$stmt -> close();
+            return "error";
 
-		$stmt = null;
+        }
 
-	}
+        $stmt->close();
+
+        $stmt = null;
+
+    }
 
 }
 
