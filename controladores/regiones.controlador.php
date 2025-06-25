8b4fdfe6bb25b927ef33869ddfb15fb3640348e3
Julián Esteban Gáez Vargas <vargasjulian.2207@gmail.com>
2025-06-25 00:50:39 -0400

    Update regiones.controlador.php

--- controladores/regiones.controlador.php
+++ controladores/regiones.controlador.php
@@ -16,19 +16,15 @@ class ControladorRegiones
     /*=============================================
     MOSTRAR COMUNAS
     =============================================*/
-    static public function ctrMostrarComunas($item, $valor)
-    {
+    static public function ctrMostrarComunas($item, $valor) {
         $tabla = "comunas"; // Tabla donde se guardan las comunas
 
         if ($item != null) {
-            // Verifica si es un número o una cadena
-            $paramType = is_numeric($valor) ? PDO::PARAM_INT : PDO::PARAM_STR;
-
-            // Consulta para obtener la comuna que coincida con el valor del item
+            // Consulta para obtener las comunas que coincidan con el valor del item
             $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
-            $stmt->bindParam(":" . $item, $valor, $paramType);
+            $stmt->bindParam(":" . $item, $valor, PDO::PARAM_INT);
             $stmt->execute();
-            return $stmt->fetch(); // Retorna solo una comuna encontrada
+            return $stmt->fetchAll(); // Retorna todas las comunas encontradas
         } else {
             // Si no hay filtro, retornar todas las comunas
             $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
